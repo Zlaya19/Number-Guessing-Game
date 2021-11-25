fcb75af9f6bab4973c2f150271d751c2a317f148
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-11-25 19:43:32 +0100

    more features and new design added

--- guessingGame.js
+++ guessingGame.js
@@ -2,6 +2,12 @@ let input = document.getElementById('input');
 const button = document.getElementById('btn');
 const output = document.getElementById('output');
 const inputDiv = document.getElementById('inputDiv');
+let wrongNumber = document.getElementById('numberOfTries');
+
+output.classList.add('classResult');
+output.innerHTML = 'good luck!';
+
+numberOfTries = 0;
 
 const randomNumb = Math.floor(Math.random() * 100);console.log(randomNumb);
 
@@ -9,12 +15,25 @@ button.addEventListener('click', () => {
 
     input = document.getElementById('input').value;
     output.classList.add('classResult');
+
+    if(input !== randomNumb){
+        numberOfTries++
+        console.log(numberOfTries)
+        wrongNumber.innerText = numberOfTries;
+    };
     
     if(input == randomNumb){
-        output.innerHTML = 'You guessed the number';
+        output.innerHTML = `You guessed the number after ${numberOfTries} tries`;
         inputDiv.classList.add('classDiv');
         inputDiv.innerHTML = 'Congratulations';
-        output.innerHTML = 'You win';
+
+    }
+
+    if(numberOfTries == 10){
+        output.remove();
+        inputDiv.remove();
+        container.classList.add('classDiv');
+        container.innerHTML = 'You lost the game';
     }
 
     if(input < randomNumb){
@@ -23,10 +42,5 @@ button.addEventListener('click', () => {
     if(input > randomNumb){
         output.innerHTML = `The number you are looking for is less then ${input}`;
     }
-
-    /* if(randomNumb - input < 10 && randomNumb - input > -10){
-        output.innerHTML = 'hot';
-        output.style.color = '#C12B1A';
-    } */
 
 });
