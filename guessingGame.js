a6ec6751c788ada0aef9c416f434d113b2c9ddd8
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-11-26 07:20:24 +0100

    comments added

--- guessingGame.js
+++ guessingGame.js
@@ -7,8 +7,10 @@ let wrongNumber = document.getElementById('numberOfTries');
 output.classList.add('classResult');
 output.innerHTML = 'good luck!';
 
+//  shows us how many times we have tried to find the number (line 22)
 numberOfTries = 0;
 
+//  Random number between 1 and 100
 const randomNumb = Math.floor(Math.random() * 100);console.log(randomNumb);
 
 button.addEventListener('click', () => {
@@ -16,19 +18,21 @@ button.addEventListener('click', () => {
     input = document.getElementById('input').value;
     output.classList.add('classResult');
 
+    //  whenever the numbers do not match, we add +1 on numbersOfTries
     if(input !== randomNumb){
         numberOfTries++
         console.log(numberOfTries)
         wrongNumber.innerText = numberOfTries;
     };
     
+    //  When the numbers match
     if(input == randomNumb){
         output.innerHTML = `You guessed the number after ${numberOfTries} tries`;
         inputDiv.classList.add('classDiv');
         inputDiv.innerHTML = 'Congratulations';
-
     }
 
+    //  After ten tries the game is over
     if(numberOfTries == 10){
         output.remove();
         inputDiv.remove();
